a0948f567d4edfc103995d66e18e504b99fcb1f6
Davronnn <kifleader@gmail.com>
2023-01-05 16:59:13 +0500

    last

---
+++ main.js
@@ -0,0 +1,12 @@
+// var name = prompt('Salom ismingiz nima');
+// var age = prompt('Yoshingiz nechida');
+
+// var result = 'Salom, ' + name + ' Yoshingiz ' + age + 'da';
+
+// alert(result);
+
+// var priceTicket = 500;
+
+// var totalPrice = priceTicket * 10650.34;
+// console.log(totalPrice);
+
