442b51a17c09b6502417cc6946c78ac4449dec08
Davronnn <kifleader@gmail.com>
2023-01-26 17:01:18 +0500

    as

--- js/home-work.js
+++
@@ -1,7 +0,0 @@
-function sayName(name = 'kimdir') {
-    
-	return 'salom ' + name;
-}
-
-console.log(sayName('John'));
-console.log(sayName());
--- js/main.js
+++ js/main.js
@@ -3,6 +3,56 @@ const ul = document.querySelector('#todos');
 const errSpan = document.querySelector('#span');
 
 const themeBtn = document.querySelector('#btn-theme');
+const todos = ["Kitob o'qish", "Futbol ko'rish", 'Bozorga borish'];
+const names = ['awdas', 'ASdasd', 'Asdasd'];
+
+function renderTodos(array) {
+	ul.textContent = '';
+	for (let i = 0; i < array.length; i++) {
+		const newLi = document.createElement('li');
+		newLi.className = 'list-group-item d-flex justify-content-between';
+		newLi.innerHTML = `
+						<h3>${array[i]}</h3>
+						<div>
+							<button class="btn btn-info text-white">
+								Completed
+							</button>
+							<button class="btn btn-danger">Delete</button>
+						</div>
+	`;
+
+		ul.appendChild(newLi);
+	}
+}
+
+renderTodos(todos);
+
+elForm.addEventListener('submit', function (evt) {
+	evt.preventDefault();
+
+	let input = evt.target.todo;
+
+	// form validation
+	if (input.value === '') {
+		span.style.display = 'block';
+		input.className += ' border-danger';
+
+		return;
+	}
+	span.style.display = 'none';
+	input.className = 'form-control';
+
+	// add todo to array
+
+	todos.push(input.value);
+
+	renderTodos(todos);
+	elForm.reset();
+});
+
+
+
+
 
 let theme = 'light';
 
@@ -18,38 +68,3 @@ themeBtn.addEventListener('click', function () {
 		themeBtn.textContent = 'Kunduzgi rejim';
 	}
 });
-
-elForm.addEventListener('submit', function (evt) {
-	evt.preventDefault();
-
-	let input = evt.target.todo;
-
-	// form validation
-	if (input.value === '') {
-		span.style.display = 'block';
-		input.className += ' border-danger';
-
-		return;
-	}
-
-	span.style.display = 'none';
-	input.className = 'form-control';
-
-	// create new todo
-	const newLi = document.createElement('li');
-	newLi.className = 'list-group-item d-flex justify-content-between';
-
-	newLi.innerHTML = `
-						<h3>${input.value}</h3>
-						<div>
-							<button class="btn btn-info text-white">
-								Completed
-							</button>
-							<button class="btn btn-danger">Delete</button>
-						</div>
-	`;
-
-	elForm.reset();
-
-	ul.appendChild(newLi);
-}); // number
