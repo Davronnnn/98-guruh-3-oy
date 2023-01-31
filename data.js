71aa26ecd5fb71d7ba823baee7b337a61cc36e58
Davronnn <kifleader@gmail.com>
2023-01-31 17:18:35 +0500

    last

--- data.js
+++ data.js
@@ -1,17 +1,20 @@
-const todos = [
+let todos = [
 	{
+		id:0,
 		title: "Kitob o'qish",
 		completed: false,
-		date: '2023-01-10',
+		date: '2013-05-3',
 	},
 	{
+		id:1,
 		title: 'yugurish',
 		completed: false,
 		date: '2023-01-10',
 	},
 	{
+		id:2,
 		title: 'uyga qaytish',
 		completed: true,
-		date: '2023-01-10',
+		date: '2023-01-25',
 	},
 ];
--- js/main.js
+++ js/main.js
@@ -4,31 +4,55 @@ const errSpan = document.querySelector('#span');
 
 const themeBtn = document.querySelector('#btn-theme');
 
+function generateDate(time) {
+	const date = new Date(time);
+	const year = date.getFullYear();
+	let month = date.getMonth() + 1;
+	let hour = date.getHours();
+	let minutes = date.getMinutes();
+	let day = date.getDate();
+
+	if (day < 10) {
+		day = '0' + day;
+	}
+	if (month < 10) {
+		month = '0' + month;
+	}
+	if (hour < 10) {
+		hour = '0' + hour;
+	}
+	if (minutes < 10) {
+		minutes = '0' + minutes;
+	}
+
+	return `${hour}:${minutes} / ${day}.${month}.${year}`;
+}
 
 function renderTodos(array) {
 	ul.textContent = '';
 
-	for (let i = 0; i < array.length; i++) {
+	array.forEach(function (todo) {
+		const resultDate = generateDate(todo.date);
 		const newLi = document.createElement('li');
 		newLi.className = 'list-group-item d-flex justify-content-between';
 		newLi.innerHTML = `
 		<div>			
-						<h3 style='${array[i].completed ? 'text-decoration:line-through' : ''}'>${
-			array[i].title
+						<h3 style='${todo.completed ? 'text-decoration:line-through' : ''}'>${
+			todo.title
 		}</h3>
-			<p class="">${array[i].date}  </p>
+			<p class=""> 🗓️ ${resultDate} </p>
 		</div>
 						<div>
-							<button class="btn btn-info text-white">
+							<button data-id=${todo.id} class="complete-btn btn btn-info text-white">
 								Completed
 							</button>
-
-							<button class="btn btn-danger">Delete</button>
+							<button data-id=${todo.id} class="btn btn-primary">Edit</button>
+							<button data-id=${todo.id}  class="btn btn-danger">Delete</button>
 						</div>
 	`;
 
 		ul.appendChild(newLi);
-	}
+	});
 }
 
 renderTodos(todos);
@@ -52,6 +76,8 @@ elForm.addEventListener('submit', function (evt) {
 
 	// add todo to array
 	const newTodo = {
+		// id:	todos.length+1,
+		id: todos.length > 0 ? todos[todos.length - 1].id + 1 : 0,
 		title: input.value,
 		completed: false,
 		date: date.value,
@@ -79,6 +105,36 @@ themeBtn.addEventListener('click', function () {
 	}
 });
 
-// array
-// function
-// object
+ul.addEventListener('click', function (evt) {
+	const element = evt.target;
+
+	if (element.className.includes('complete-btn')) {
+		const id = Number(element.dataset.id);
+
+		for (let i = 0; i < todos.length; i++) {
+			const todo = todos[i];
+
+			if (todo.id === id) {
+				todo.completed = !todo.completed;
+			}
+		}
+
+		renderTodos(todos);
+	}
+
+	if (element.className.includes('btn-danger')) {
+		const id = Number(element.dataset.id);
+
+		const result = [];
+		for (let i = 0; i < todos.length; i++) {
+			const todo = todos[i];
+
+			if (todo.id !== id) {
+				result.push(todo);
+			}
+		}
+
+		todos = result;
+		renderTodos(todos);
+	}
+});
